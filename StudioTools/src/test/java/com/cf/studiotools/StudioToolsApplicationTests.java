cdcd4a754e55a37041bc49d788a397ed19edf0df
不正经 <wangjiajin0920@163.com>
2025-01-03 14:56:45 +0800

    项目创建

---
+++ StudioTools/src/test/java/com/cf/studiotools/StudioToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.studiotools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
