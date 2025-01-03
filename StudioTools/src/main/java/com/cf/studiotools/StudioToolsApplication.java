895da9977b73d46bb8734c9cac85110ad9757130
不正经 <wangjiajin0920@163.com>
2025-01-03 14:56:45 +0800

    项目创建

---
+++ StudioTools/src/main/java/com/cf/studiotools/StudioToolsApplication.java
@@ -0,0 +1,13 @@
+package com.cf.studiotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudioToolsApplication.class, args);
+    }
+
+}
