49a48374178bbbc851bcf82f5e652f398393f32e
不正经 <wangjiajin0920@163.com>
2025-01-03 19:03:10 +0800

    添加swagger

--- StudioTools/src/main/java/com/cf/studio/StudioToolsApplication.java
+++ StudioTools/src/main/java/com/cf/studio/StudioToolsApplication.java
@@ -1,9 +1,11 @@
-package com.cf.studiotools;
+package com.cf.studio;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class StudioToolsApplication {
 
     public static void main(String[] args) {
--- StudioTools/src/test/java/com/cf/studio/StudioToolsApplicationTests.java
+++ StudioTools/src/test/java/com/cf/studio/StudioToolsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cf.studiotools;
+package com.cf.studio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
