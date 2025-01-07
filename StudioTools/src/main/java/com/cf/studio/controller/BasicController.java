9de5882d76dc2f28c0ddca442a188abb82fbc181
不正经 <wangjiajin0920@163.com>
2025-01-07 16:15:55 +0800

    添加响应式登录界面

--- StudioTools/src/main/java/com/cf/studio/controller/BasicController.java
+++ StudioTools/src/main/java/com/cf/studio/controller/BasicController.java
@@ -2,10 +2,49 @@ package com.cf.studio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class BasicController {
-
-
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+    @RequestMapping("/index")
+    public String index1(){
+        return "index";
+    }
+    @RequestMapping("/active")
+    public String active(){
+        return "active";
+    }
+    @RequestMapping("/category")
+    public String category(){
+        return "category";
+    }
+    @RequestMapping("/particulars")
+    public String particulars(){
+        return "particulars";
+    }
+    @RequestMapping("/my")
+    public String my(){
+        return "my";
+    }
+    @RequestMapping("/us")
+    public String us(){
+        return "us";
+    }
+    @RequestMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
 }
